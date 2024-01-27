ac5a7a6ac50f88ecf947a86cbb565ebbe7748623
ebny-buniad <ebnybuniad@gmail.com>
2024-01-27 21:40:10 +0600

    version updated

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,39 +1,14 @@
-// scroll
-var scrollWindow = function() {
-    $(window).scroll(function(){
-        var $w = $(this),
-                st = $w.scrollTop(),
-                navbar = $('.ftco_navbar'),
-                sd = $('.js-scroll-wrap');
+// Navbar Active Color 
+$(document).ready(function () {
+    $("ul.navbar-nav > li > a").click(
+      function (e) {
+        $("ul.navbar-nav > li").removeClass(
+          "active");
+        $("ul.navbar-nav > li > a")
+        .css("color", "")
+        ;
 
-        if (st > 150) {
-            if ( !navbar.hasClass('scrolled') ) {
-                navbar.addClass('scrolled');	
-            }
-        } 
-        if (st < 150) {
-            if ( navbar.hasClass('scrolled') ) {
-                navbar.removeClass('scrolled sleep');
-            }
-        } 
-        if ( st > 350 ) {
-            if ( !navbar.hasClass('awake') ) {
-                navbar.addClass('awake');	
-            }
-            
-            if(sd.length > 0) {
-                sd.addClass('sleep');
-            }
-        }
-        if ( st < 350 ) {
-            if ( navbar.hasClass('awake') ) {
-                navbar.removeClass('awake');
-                navbar.addClass('sleep');
-            }
-            if(sd.length > 0) {
-                sd.removeClass('sleep');
-            }
-        }
-    });
-};
-scrollWindow();
+        $(this).addClass("active");
+        $(this).css("color", "#ff007f");
+      });
+  });
